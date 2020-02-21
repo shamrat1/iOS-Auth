7f0eef036343531bfd35e5417abe93d8c631e7be
Yasin Shamrat <yshamrat@gmail.com>
2020-02-22 01:04:37 +0600

    login screen

--- iOS-Auth/ViewController.swift
+++ iOS-Auth/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var passwordOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func onClickLogin(_ sender: UIButton) {
+        
+        if emailOutlet != nil && passwordOutlet != nil {
+            print("\(emailOutlet.text) \(passwordOutlet.text)")
+        }
+    }
+    
 
 }
 
